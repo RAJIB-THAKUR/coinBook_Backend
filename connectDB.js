31ecc9ba1d56ee177ca998717fa03489b081133e
xyz <rajsharma1234@gmail.com>
2023-03-18 12:47:53 +0530

    DB_Name_Changed_18/03/23

--- connectDB.js
+++ connectDB.js
@@ -4,7 +4,7 @@ mongoose.set("strictQuery", false);
 //Function to Connect MongoDB
 const connect_MongoDB = () =>
   mongoose
-    .connect("mongodb+srv://rajibthakur:rajibthakur@cluster0.zd9jgxo.mongodb.net/test", {
+    .connect("mongodb+srv://rajibthakur:rajibthakur@cluster0.zd9jgxo.mongodb.net/coin_Book_App", {
       useNewUrlParser: true,
     })
     .then(() => {
